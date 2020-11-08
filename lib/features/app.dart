8a02626db80ec3853114e4e235f8cf25ac7168ce
Bodnár Renátó <renato.bodnar@kuka.com>
2020-11-08 20:23:01 +0100

    fcm messaging implemented

--- lib/features/app.dart
+++ lib/features/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uni_miskolc_datashare/core/injector/injector.dart';
@@ -31,7 +32,37 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+    print(data);
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+    print(notification);
+  }
+
+  // Or do other work.
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  final fireBaseMessaging = FirebaseMessaging();
+  String fcmToken;
+
+  void setToken(token) {
+    fcmToken = token;
+    print('TOKEEEEEEEEEN:::' + fcmToken);
+  }
+
+  @override
+  void initState() {
+    fireBaseMessaging.getToken().then((value) => setToken(value));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'features/app.dart';
@@ -6,6 +7,5 @@ import 'core/injector/injector.dart' as injector;
 
 Future<void> main() async {
   injector.init();
-
   runApp(App());
 }
